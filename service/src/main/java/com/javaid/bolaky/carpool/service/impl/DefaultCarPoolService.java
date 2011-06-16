6b7c97ed68604f5e15e1ba914388861ec6a97ed5
javaid <javaid@javaid>
2011-06-16 21:32:24 +0400

    carpool service added

---
+++ service/src/main/java/com/javaid/bolaky/carpool/service/impl/DefaultCarPoolService.java
@@ -0,0 +1,7 @@
+package com.javaid.bolaky.carpool.service.impl;
+
+import com.javaid.bolaky.carpool.service.api.CarPoolService;
+
+public class DefaultCarPoolService implements CarPoolService {
+
+}
