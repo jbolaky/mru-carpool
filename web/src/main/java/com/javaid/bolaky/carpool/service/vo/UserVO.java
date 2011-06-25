7516212f81eacfa98480f14d0e038a6ff8323d95
javaid <javaid@javaid>
2011-06-25 23:13:09 +0400

    bean added

---
+++ web/src/main/java/com/javaid/bolaky/carpool/service/vo/UserVO.java
@@ -0,0 +1,195 @@
+package com.javaid.bolaky.carpool.service.vo;
+
+public class UserVO {
+
+	private String username;
+
+	private String ageGroup;
+
+	private String firstname;
+
+	private String lastname;
+
+	private String password;
+
+	private String confirmPassword;
+
+	private String emailAddress;
+
+	private String confirmEmailAddress;
+
+	private Boolean carOwner;
+
+	private Boolean validLicense;
+
+	private String gender;
+
+	private String country;
+
+	private String district;
+
+	private String region;
+
+	private String addressLine1;
+
+	private String phoneNumber;
+
+	private Boolean allowToReceiveUpdates;
+
+	private Boolean shareCost;
+
+	private Boolean shareDriving;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getAgeGroup() {
+		return ageGroup;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public String getConfirmEmailAddress() {
+		return confirmEmailAddress;
+	}
+
+	public Boolean getCarOwner() {
+		return carOwner;
+	}
+
+	public Boolean getValidLicense() {
+		return validLicense;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public Boolean getAllowToReceiveUpdates() {
+		return allowToReceiveUpdates;
+	}
+
+	public Boolean getShareCost() {
+		return shareCost;
+	}
+
+	public Boolean getShareDriving() {
+		return shareDriving;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setAgeGroup(String ageGroup) {
+		this.ageGroup = ageGroup;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public void setConfirmEmailAddress(String confirmEmailAddress) {
+		this.confirmEmailAddress = confirmEmailAddress;
+	}
+
+	public void setCarOwner(Boolean carOwner) {
+		this.carOwner = carOwner;
+	}
+
+	public void setValidLicense(Boolean validLicense) {
+		this.validLicense = validLicense;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public void setAllowToReceiveUpdates(Boolean allowToReceiveUpdates) {
+		this.allowToReceiveUpdates = allowToReceiveUpdates;
+	}
+
+	public void setShareCost(Boolean shareCost) {
+		this.shareCost = shareCost;
+	}
+
+	public void setShareDriving(Boolean shareDriving) {
+		this.shareDriving = shareDriving;
+	}
+
+}
