3c6cb5171802697f0609dd61f30abdbe77b4fd3b
javaid.bolaky <javaid.bolaky@gmail.com>
2011-07-16 23:16:01 +0400

    config refactored

--- domains/pools/pools.entity/src/main/java/com/javaid/bolaky/domain/pools/repository/api/PoolsRepository.java
+++ domains/pools/pools.entity/src/main/java/com/javaid/bolaky/domain/pools/repository/api/PoolsRepository.java
@@ -1,12 +1,12 @@
 package com.javaid.bolaky.domain.pools.repository.api;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import com.javaid.bolaky.domain.pools.entity.Pool;
 
-public interface PoolsRepository extends JpaRepository<Pool, Long> {
+public interface PoolsRepository extends CrudRepository<Pool, Long> {
 
 	@Query(value = "select p from Pool p where p.username =:username")
 	Pool findByUsername(@Param("username") String username);
