6c484a6888a3511b9e77dc70d7acda5f614ec492
javaid.bolaky <javaid.bolaky@gmail.com>
2011-07-30 23:59:59 +0400

    find pools method added

--- domains/pools/pools.service/src/main/java/com/javaid/bolaky/domain/pools/service/api/PoolsService.java
+++ domains/pools/pools.service/src/main/java/com/javaid/bolaky/domain/pools/service/api/PoolsService.java
@@ -1,10 +1,15 @@
 package com.javaid.bolaky.domain.pools.service.api;
 
+import java.util.List;
+
 import com.javaid.bolaky.domain.pools.entity.Pool;
+import com.javaid.bolaky.domain.pools.track.vo.PoolSearchCriteria;
 
 public interface PoolsService {
 
 	Pool save(Pool pool);
 	
 	Pool find(String username);
+	
+	List<Pool> findPools(PoolSearchCriteria poolSearchCriteria);
 }
--- domains/pools/pools.service/src/main/java/com/javaid/bolaky/domain/pools/service/impl/DefaultPoolsService.java
+++ domains/pools/pools.service/src/main/java/com/javaid/bolaky/domain/pools/service/impl/DefaultPoolsService.java
@@ -1,5 +1,7 @@
 package com.javaid.bolaky.domain.pools.service.impl;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.transaction.annotation.Propagation;
@@ -8,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.javaid.bolaky.domain.pools.entity.Pool;
 import com.javaid.bolaky.domain.pools.repository.api.PoolsRepository;
 import com.javaid.bolaky.domain.pools.service.api.PoolsService;
+import com.javaid.bolaky.domain.pools.track.vo.PoolSearchCriteria;
 
 @Transactional(readOnly = true)
 public class DefaultPoolsService implements PoolsService {
@@ -24,6 +27,11 @@ public class DefaultPoolsService implements PoolsService {
 	public Pool find(String username) {
 
 		return poolsRepository.findByUsername(username);
+	}
+
+	public List<Pool> findPools(PoolSearchCriteria poolSearchCriteria) {
+
+		return poolsRepository.findPool(poolSearchCriteria);
 	}
 
 }
